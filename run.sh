ba339ba3abd3dc83c5e23a8ef9f5340257a23029
DanNicolaeBoca <dan.nicolae.boca01@gmail.com>
2025-12-30 00:14:12 +0200

    add env vars for homebrew after install

--- run.sh
+++ run.sh
@@ -93,6 +93,7 @@ else
   
   echo "Install brew"
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  [ -d /home/linuxbrew/.linuxbrew ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
   brew insall gcc zsh-syntax-highlighting zsh-autosuggestions
 
   # Some programs just run better as flatpaks. Like discord/spotify
