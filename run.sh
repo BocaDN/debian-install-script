244e29c08ed3511127d9622c9c40ed5ed162ad5f
DanNicolaeBoca <dan.nicolae.boca01@gmail.com>
2025-12-30 18:08:16 +0200

    edit

--- run.sh
+++ run.sh
@@ -77,6 +77,8 @@ nvm_setup() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
     )
   fi
+  export NVM_DIR="$HOME/.nvm"
+  source "$NVM_DIR/nvm.sh"
   nvm install node
   nvm use node
 }
